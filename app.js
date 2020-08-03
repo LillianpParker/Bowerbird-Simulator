1a1fdc323c3fcafa729d9306c2bf90927ceb2944
Lillian Parker <Lillianpparker@outlook.com>
2020-08-03 17:12:34 -0500

    animations done, css coming together on first page

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 // $(() => {
+// Directions Screen Animations
+
+// const Kami = $("#Kami");
+// const Speech = $("#Speech");
+
+// The the user hovers the mouse over
+// the rectangle the "expand-bounce"
+//animation is called and the duration
+// is set to 250 miliseconds.
+// When the user moves the mouse away
+// the "shrink" animation gets called
+// and the duration is set to 100
+// miliseconds.
+// Kami.hover(
+//     function () {
+//         Speech.css({
+//             "animation-name": "expand-bounce",
+//             "animation-duration": "0.25s"
+//         });
+//     },
+//     function () {
+//         Speech.css({
+//             "animation-name": "shrink",
+//             "animation-duration": "0.1s"
+//         });
+//     }
+// );
 
 // Item Array
 
@@ -28,6 +55,7 @@ function playerSubset(list, numChoices) {
     }
     return pickedItems
 }
+
 let compPoints = 0
 let points = 0
 let round = 1
@@ -35,13 +63,13 @@ let totalRounds = 10
 
 function playerSearch(e) {
     round = round + 1
-        let roundNumber = $('<h1>') 
-        roundNumber.text("Round: " + round)
-        roundNumber.appendTo('#scoreValue')
+    let roundNumber = $('<h1>')
+    roundNumber.text("Round: " + round)
+    roundNumber.appendTo('#scoreValue')
     let choices = playerSubset(items, 3)
     for (let i = 0; i < choices.length; i++) {
         let buttonChoice = $('<button>')
-        buttonChoice.text(choices[i].name + ": " + " " + choices[i].value)
+        buttonChoice.text(choices[i].name)
         buttonChoice.appendTo($('#choices'))
         buttonChoice.click(() => {
             // Player Points
@@ -51,23 +79,27 @@ function playerSearch(e) {
             scoreCard.text(points)
             $('#choices').empty()
             // Computer Points
-            compPoints = compPoints + getComputerChoice(compItem.value)
+            compPoints = compPoints + getComputerChoice().value
             let compScoreCard = $('#compScoreValue')
             compScoreCard.appendTo('#compScoreValue')
             console.log(compPoints)
-            })
-    if (round >= totalRounds ) {
+        })
+
+    }
+    if (points > compPoints && round >= totalRounds) {
         winningScreen()
     }
-}
+    if (points < compPoints && round >= totalRounds) {
+        losingScreen()
+    }
 }
 
 // Computer Below
 
-function getComputerChoice(list) {
+function getComputerChoice() {
     let compIndex = Math.floor(Math.random() * items.length)
     let compItem = items[compIndex]
-    return [compItem]
+    return compItem
 
 }
 
@@ -75,8 +107,25 @@ function computerSearch(e) {
     console.log(getComputerChoice(items))
 }
 
+function winningScreen() {
+    $('#removeForEnd').empty()
+    let winningTitle = $('<h1>')
+    winningTitle.text("Congratulations, your Bower has ranked in first! You've won her attention with a most stunning and colorful bower, and beat your competitor with a final rating of "+ playerSearch().points)
+    winningTitle.appendTo('body')
+    winningTitle.append("<img id='bowerImage' src='https://blogs.zeiss.com/sports-optics/birding/en/wp-content/uploads/sites/2/2018/05/Laubenvogel_Nestbau.jpg'/>");
+}
 
-
-
+function losingScreen() {
+    $('#removeForEnd').empty()
+    let losingTitle = $('<h1>')
+    losingTitle.text("It would appear your competitors bower looked better than yours. But good news is, you can try with another lady if you'd like, or, if you're so heartbroken, you can wait until next season. What do ya say?")
+    losingTitle.appendTo('#removeForEnd')
+    let tryAgain = $('<button>')
+    tryAgain.text("Try Again")
+    tryAgain.appendTo(losingTitle)
+    let givingUp = $('<button>')
+    givingUp.text("I'll wait till next seson")
+    givingUp.appendTo(losingTitle)
+}
 
 // })
